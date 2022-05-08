546e9d9b49d4b3f9089515d67a88f581913f3e01
Nobumasa-Izuta <izuta.nobumasa@icloud.com>
2022-05-08 15:51:52 +0900

    課題6 コメント機能の非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,31 +1,20 @@
 class BookCommentsController < ApplicationController
-  before_action :get_show_variables
-
   def create
     book_comment = BookComment.new(book_comment_params)
+    book_comment.save
 
-    if book_comment.save
-      redirect_to params[:return_url].presence || book_path(book_comment.book_id)
-    else
-      render 'books/show'
-    end
+    @book = Book.includes(:favorites, book_comments: { user: { profile_image_attachment: :blob } }).find(params[:book_id])
   end
+  
 
   def destroy
     book_comment = BookComment.find(params[:id])
+    book_comment.destroy
 
-    if book_comment && book_comment.destroy
-      redirect_to params[:return_url].presence || book_path(book_comment.book_id)
-    else
-      render 'books/show'
-    end
+    @book = Book.includes(:favorites, book_comments: { user: { profile_image_attachment: :blob } }).find(params[:book_id])
   end
 
   private
-
-  def get_show_variables
-    @book = Book.includes(:favorites, book_comments: { user: { profile_image_attachment: :blob } }).find(params[:book_id])
-  end
 
   def book_comment_params
     params.require(:book_comment).permit(:book_id, :user_id, :comment)
